90952a20d84909e7e12cd6833cd8c083a9c7077a
SinkLineP <popovartem505@gmail.com>
2021-08-02 16:46:36 +0300

    update 1 add two chance

--- script/index.js
+++ script/index.js
@@ -1,5 +1,6 @@
 const yes = document.getElementById("alert-yes");
 const no = document.getElementById("alert-no");
+const reload = document.getElementById("alert-reload");
 const text = document.getElementById("daun");
 
 
@@ -7,12 +8,17 @@ function yesBtn() {
     text.innerText = "Я так и знал)"
     yes.style.display = "none"
     no.style.display = "none"
+    reload.style.display = "block"
 }
 
 function noBtn() {
     const rand = Math.floor(Math.random() * 101);
     no.style.transform = "translateX("+ `${rand}`+ "px) translateY("+ `${rand}`+ "px)"
     // no.style.transform = "translateY(100px)"
+}
+
+function reloadPage() {
+    location.reload()
 }
 
 no.addEventListener("click", () => {
@@ -22,4 +28,9 @@ no.addEventListener("click", () => {
 yes.addEventListener("click", () => {
     yesBtn()
 })
+
+reload.addEventListener("click", () => {
+    reloadPage()
+})
+
 
